f468460726489e3ca53d3bee418029aab801f512
Pprud <Pavel.Prudky@Accolade.com>
2019-11-29 10:52:16 +0100

    init

--- csv_to_parquet_converter/__main__.py
+++ csv_to_parquet_converter/__main__.py
@@ -1,57 +1,68 @@
 import argparse
-
 import pandas as pd
-#import modin.pandas as pd
+# import modin.pandas as pd
+from csv_to_parquet_converter.lib import aws
 
 
-def converter(source_file_path, target_file_path, compression=None, columns_subset=None):
+def converter(source_file_path, target_file_path, columns_subset=None, compression=None):
     """
     function to convert source csv file to target parquet file
     :param source_file_path:
     :param target_file_path:
-    :param compression:
     :param columns_subset:
+    :param compression:
     :return:
     """
-    df = pd.read_csv(source_file_path,
-                     usecols=columns_subset)
+    df = None
 
-    df.to_parquet(target_file_path, compression=compression or 'UNCOMPRESSED', engine='fastparquet')
+    if str(source_file_path).startswith("s3://"):
+        s3obj = aws.init_s3()
+        aws.download_from_s3(s3obj, source_file_path, '/temp/xxx.csv')
+        source_file_path = '/temp/xxx.csv'
+
+    try:
+        df = pd.read_csv(source_file_path,
+                         usecols=columns_subset)
+    except Exception as e:
+        print(str(e))
+
+    try:
+        df.to_parquet(target_file_path,
+                      compression=compression or 'UNCOMPRESSED',
+                      engine='fastparquet')
+    except Exception as e:
+        print(str(e))
+
+    if str(target_file_path).startswith("s3://"):
+        s3obj = aws.init_s3()
+        aws.upload_to_s3(s3obj, target_file_path, '/temp/yyy.parquet')
 
     return 0
 
-def prepare_args():
-    ###########################################################################################
-    """ 0: The arguments parser and main launcher """
-    ###########################################################################################
 
+def prepare_args():
+    """
+
+    :return:
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-dn', '--databasename', type=str, required=True)
-    parser.add_argument('-sn', '--schemaname', type=str, required=True)
-    parser.add_argument('-td', '--targetdirectory', type=str, required=True)
-    parser.add_argument('-dr', '--dryrun', type=str, required=True)
+    parser.add_argument('-sfp', '--sourcefilepath', type=str, required=True)
+    parser.add_argument('-tfp', '--targetfilepath', type=str, required=True)
+    parser.add_argument('-cols', '--columnssubset', type=str, required=False)
+    parser.add_argument('-comp', '--compression', type=str, required=False)
     parsed = parser.parse_args()
 
-    database_name = parsed.databasename
-    schema_name = parsed.schemaname
+    source_file_path = parsed.sourcefilepath
+    target_file_path = parsed.targetfilepath
+    columns_subset = parsed.columnssubset
+    compression = parsed.compression
 
-    target_directory = parsed.targetdirectory
-    target_directory = target_directory.replace('\f', '\\f')
-
-    dry_run = parsed.dryrun
-    # arg parse bool data type known bug workaround
-    if dry_run.lower() in ('no', 'false', 'f', 'n', 0):
-        dry_run = False
-        dry_run_str_prefix = ''
-    else:
-        dry_run = True
-        dry_run_str_prefix = 'Dry run '
-
-    # obj = Runner(database_name, schema_name, target_directory, dry_run, dry_run_str_prefix)
-    # Runner.main(obj)
-    converter(source_file_path="C:\GIT_PROJECTS_PERSONAL\csv_to_parquet_converter\csv_to_parquet_converter\scratch\\source.csv",
-              target_file_path="C:\GIT_PROJECTS_PERSONAL\csv_to_parquet_converter\csv_to_parquet_converter\scratch\\target.parquet",
-              columns_subset=["col1", "col3"])
+    converter(source_file_path=source_file_path,
+              target_file_path=target_file_path,
+              columns_subset=columns_subset,
+              # columns_subset=["col1", "col3"]
+              compression=compression
+              )
 
 
 if __name__ == '__main__':
