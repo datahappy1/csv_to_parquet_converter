c0ed4bbe0b9142a829ed13e8157987973739a106
Pprud <Pavel.Prudky@Accolade.com>
2019-12-04 12:30:22 +0100

    init

--- csv_to_parquet_converter/__main__.py
+++ csv_to_parquet_converter/__main__.py
@@ -1,17 +1,21 @@
+"""
+__main__ module
+"""
+
 import argparse
 import logging
 import traceback
 import pandas as pd
-# import modin.pandas as pd
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
-def converter_csv_to_parquet(source_file_path, target_file_path, columns_subset=None, compression=None):
+def converter_csv_to_parquet(source_file_path, target_file_path,
+                             columns_subset=None, compression=None):
     """
     function to convert source csv file to target parquet file
     :param source_file_path:
@@ -20,29 +24,30 @@ def converter_csv_to_parquet(source_file_path, target_file_path, columns_subset=
     :param compression:
     :return:
     """
-    df = None
+    data_frame = None
 
     try:
-        df = pd.read_csv(source_file_path,
-                         usecols=columns_subset)
-        logger.info("read_csv passed")
+        data_frame = pd.read_csv(source_file_path,
+                                 usecols=columns_subset)
+        LOGGER.info("read_csv passed")
     except Exception:
-        tb = traceback.format_exc()
-        logger.error(tb)
+        trace_back = traceback.format_exc()
+        LOGGER.error(trace_back)
 
     try:
-        df.to_parquet(target_file_path,
-                      compression=compression or 'UNCOMPRESSED',
-                      engine='fastparquet')
-        logger.info("to_parquet passed")
+        data_frame.to_parquet(target_file_path,
+                              compression=compression or 'UNCOMPRESSED',
+                              engine='fastparquet')
+        LOGGER.info("to_parquet passed")
     except Exception:
-        tb = traceback.format_exc()
-        logger.error(tb)
+        trace_back = traceback.format_exc()
+        LOGGER.error(trace_back)
 
     return 0
 
 
-def converter_parquet_to_csv(source_file_path, target_file_path, columns_subset=None):
+def converter_parquet_to_csv(source_file_path, target_file_path,
+                             columns_subset=None):
     """
     function to convert source parquet file to target csv file
     :param source_file_path:
@@ -51,37 +56,50 @@ def converter_parquet_to_csv(source_file_path, target_file_path, columns_subset=
     :param compression:
     :return:
     """
-    df = None
+    data_frame = None
 
     try:
-        df = pd.read_parquet(source_file_path,
-                             engine='fastparquet',
-                             columns=columns_subset)
-        logger.info("read_parquet passed")
+        data_frame = pd.read_parquet(source_file_path,
+                                     engine='fastparquet',
+                                     columns=columns_subset)
+        LOGGER.info("read_parquet passed")
     except Exception:
-        tb = traceback.format_exc()
-        logger.error(tb)
+        trace_back = traceback.format_exc()
+        LOGGER.error(trace_back)
 
     try:
-        df.to_csv(target_file_path, index=False)
-        logger.info("to_csv passed")
+        data_frame.to_csv(target_file_path, index=False)
+        LOGGER.info("to_csv passed")
     except Exception:
-        tb = traceback.format_exc()
-        logger.error(tb)
+        trace_back = traceback.format_exc()
+        LOGGER.error(trace_back)
 
     return 0
 
 
-def main(conversion_type, source_file_path, target_file_path, columns_subset, compression):
+def main(conversion_type, source_file_path, target_file_path,
+         columns_subset, compression):
+    """
+    main conversion runner
+    :param conversion_type:
+    :param source_file_path:
+    :param target_file_path:
+    :param columns_subset:
+    :param compression:
+    :return:
+    """
     if conversion_type == 'csv_to_parquet':
-        conversion_result = converter_csv_to_parquet(source_file_path, target_file_path, columns_subset, compression)
+        conversion_result = converter_csv_to_parquet(source_file_path, target_file_path,
+                                                     columns_subset, compression)
     elif conversion_type == 'parquet_to_csv':
-        conversion_result = converter_parquet_to_csv(source_file_path, target_file_path, columns_subset)
+        conversion_result = converter_parquet_to_csv(source_file_path, target_file_path,
+                                                     columns_subset)
     else:
         raise NotImplementedError
 
     if conversion_result == 0:
         return 0
+    return 1
 
 
 def prepare_args():
@@ -115,7 +133,10 @@ def prepare_args():
                        compression=compression
                        )
     if main_result == 0:
-        logger.info('Conversion end to end passed')
+        LOGGER.info('Conversion end to end passed')
+    elif main_result == 1:
+        LOGGER.info('Conversion end to end failed')
+
 
 if __name__ == '__main__':
     prepare_args()
--- csv_to_parquet_converter/tests/test_functional.py
+++ csv_to_parquet_converter/tests/test_functional.py
@@ -1,7 +1,22 @@
-import pytest
-
 from csv_to_parquet_converter.__main__ import converter_csv_to_parquet, converter_parquet_to_csv
 
-def test_functional():
-    # TODO
-    pass
+
+def test_functional_csv_to_parquet():
+    """
+    functional test function for local csv to parquet
+    :return: assertion output
+    """
+    calculated_result = converter_csv_to_parquet(source_file_path='/files/source.csv',
+                                                 target_file_path='/files/target.parquet')
+
+    assert calculated_result == 0
+
+def test_functional_parquet_to_csv():
+    """
+    functional test function for local parquet to csv
+    :return: assertion output
+    """
+    calculated_result = converter_parquet_to_csv(source_file_path='/files/source.parquet',
+                                                 target_file_path='/files/target.csv')
+
+    assert calculated_result == 0
